f6eee52024de8ac32c20db9efef37a5057cfb933
Yoan Malaver <yoamalaver@gmail.com>
2020-08-30 21:42:34 -0500

    Service Workers Experience offline

--- assets/index.js
+++ assets/index.js
@@ -13,3 +13,9 @@ button.onclick = () => player.togglePlay();
 
 const mute = document.querySelector('.buttonMute');
 mute.onclick = () => player.toggleMute();
+
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('/sw.js').catch((error) => {
+    console.log(error.message);
+  });
+}
