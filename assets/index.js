63772a6b2722f7f4017de7b568c04a30d2a11a16
Yoan Malaver <yoamalaver@gmail.com>
2020-09-04 10:23:41 -0500

    tipando con TypeScript AutoPause

--- assets/index.js
+++ assets/index.js
@@ -1,6 +1,6 @@
 import MediaPlayer from './MediaPlayer.js';
 import AutoPlay from './Plugins/AutoPlay.js';
-import AutoPause from './Plugins/AutoPause.js';
+import AutoPause from './Plugins/AutoPause.ts';
 
 const video = document.querySelector('video');
 const player = new MediaPlayer({
--- sw.js
+++ sw.js
@@ -21,14 +21,14 @@ self.addEventListener('fetch', (event) => {
 async function precache() {
   const cache = await caches.open(VERSION);
   return cache.addAll([
-    '/',
-    '/index.html',
-    '/assets/index.js',
-    '/assets/MediaPlayer.js',
-    '/assets/Plugins/AutoPlay.js',
-    '/assets/Plugins/AutoPause.js',
-    '/assets/index.css',
-    '/assets/ejercicio.mp4',
+    // '/',
+    // '/index.html',
+    // '/assets/index.js',
+    // '/assets/MediaPlayer.js',
+    // '/assets/Plugins/AutoPlay.js',
+    // '/assets/Plugins/AutoPause.js',
+    // '/assets/index.css',
+    // '/assets/ejercicio.mp4',
   ]);
 }
 
