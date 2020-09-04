c4f2195f483284bb85d297e26566f439f3c5d138
Yoan Malaver <yoamalaver@gmail.com>
2020-09-04 01:28:45 -0500

    Funciones en TypeScript

--- typescript/index.ts
+++ typescript/index.ts
@@ -49,3 +49,30 @@ comodin = { type: 'wildcard' };
 
 //object
 let someObject: object = { type: 'wildcard' };
+
+//Funciones
+function add(a: number, b: number): number {
+  return a + b;
+}
+
+const sum = add(4, 6);
+
+//funciones que regresan otras funciones
+
+function createAdder(a: number): (number) => number {
+  return function (b: number) {
+    return b + a;
+  };
+}
+
+const addFour = createAdder(4);
+const fourPlus6 = addFour(6);
+('');
+// function fullName(firstName: string, lastName: string = 'Smith): string { Valor por omision 'smith'
+function fullName(firstName: string, lastName?: string): string {
+  return `${firstName} ${lastName}`;
+}
+
+//const yoan = fullName('Yoan', 'Malaver');
+const yoan = fullName('Yoan');
+console.log(yoan);
