838a22f8c4293398c77cc976e081ed72188cd906
Yoan Malaver <yoamalaver@gmail.com>
2020-09-08 16:16:50 -0500

    implememntacion de Plugin Ads

--- assets/index.ts
+++ assets/index.ts
@@ -1,11 +1,12 @@
 import MediaPlayer from './MediaPlayer';
 import AutoPlay from './Plugins/AutoPlay';
 import AutoPause from './Plugins/AutoPause';
+import Ads from './Plugins/Ads';
 
 const video = document.querySelector('video');
 const player = new MediaPlayer({
   el: video,
-  plugins: [new AutoPlay(), new AutoPause()],
+  plugins: [new AutoPlay(), new AutoPause(), new Ads()],
 });
 
 const button: HTMLElement = document.querySelector('button');
